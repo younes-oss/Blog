95c9163b6ba5130ae9c9a1966cb53bf617d402f2
younes-oss <aitchattouyounes@gmail.com>
2025-04-23 13:28:13 +0200

    second commit

---
+++ src/app/composant/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  
+  
+}
