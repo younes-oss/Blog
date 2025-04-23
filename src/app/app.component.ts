d7bc837c47adcaf9f52588102fa78431ba44cacd
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-23 17:17:22 +0100

    i create the home template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './features/navbar/navbar.component';
 import {LoginComponent} from './features/login/login.component';
+import {HomeComponent} from './features/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,LoginComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
