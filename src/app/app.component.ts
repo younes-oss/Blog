e36e2dd0e390c829a4cbcc2d0b6325da46a5db5e
younes-oss <aitchattouyounes@gmail.com>
2025-04-24 13:48:31 +0200

    creation post-detail composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './features/navbar/navbar.component';
 import {LoginComponent} from './features/login/login.component';
 import {HomeComponent} from './features/home/home.component';
+import { PostDetailComponent } from './features/posts/post-detail/post-detail.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, LoginComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, HomeComponent,PostDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
