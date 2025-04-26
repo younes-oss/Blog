fd02db4a6985cbca4adb8e2ad6d993c9d2857290
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-26 12:40:53 +0100

    add the guard form login using canativeted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './features/navbar/navbar.component';
 import {LoginComponent} from './features/login/login.component';
 import {HomeComponent} from './features/home/home.component';
-<<<<<<< HEAD
 import { PostDetailComponent } from './features/posts/post-detail/post-detail.component';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, LoginComponent, HomeComponent,PostDetailComponent],
-=======
 import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, LoginComponent, HomeComponent, FooterComponent],
->>>>>>> b5bc2f6 (create footer)
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
