5e349d181cbe423fd9fb154ff89b25e5278b6ff3
mohamed <iaazi.mohamed.1998@gmail.com>
2025-04-28 00:00:41 +0100

    fix error

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth } from '@angular/fire/auth';
-import { provideFirestore } from '@angular/fire/firestore';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
-const appConfig = {
-  providers: [
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), // Initialize Firebase with environment config
-    provideAuth(() => getAuth()), // Provide Firebase Auth instance
-    provideFirestore(() => getFirestore()), // Provide Firestore instance
-  ]
-};
-
-bootstrapApplication(AppComponent, appConfig);
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
